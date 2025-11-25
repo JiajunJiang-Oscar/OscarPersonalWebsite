c781b091c701c5493c566322c3e3ead5102e9f07
Oscar Jiang <jiajun.jiang@my.jcu.edu.au>
2025-11-25 18:01:40 +0800

    Commit the index page (HomePage.html), create a js file to store texts.

---
+++ Content.js
@@ -0,0 +1,10 @@
+const TEXT = {
+  heroTitle: "EMBARK ON AN ADVENTURE",
+  heroSubtitle: "Explore our insights →",
+
+  navHome: "Home",
+  navAbout: "About",
+  navBlog: "Blog",
+
+  brandName: "BUTE",
+};
